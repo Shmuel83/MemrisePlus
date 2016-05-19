8cc90d46d236f104366b5eb2ab2f0eb964cdb290
Shmuel83 <rodolphe.moulin@laposte.net>
2016-05-19 15:02:06 +0200

    Reload Courses
    Fixed issue when user Reload

--- js/courses.js
+++ js/courses.js
@@ -1,6 +1,7 @@
 //{courses:[category:{"photo": "https://d1..."}, "ignored": 1, "percent_complete": 49, "description": "Basic...", "url": "http://www...", "photo": "http://d2rhek...", "goal": {"course_id": 450, "streak": 5, "points": 1266, "goal": 1500}, "audio_mode": false, "ltm": 164, "review": 8, "learned": 172, "difficult": 19, "id": 450, "num_things": 349, "name": "Bas..."},
 function coursesView() {
 var courses = JSON.parse(localStorage['courses']).courses;
+$('#divCourses').html("");
 for(var icourse=0; icourse<courses.length; icourse++) {
 	var nameCourse = courses[icourse].name;
 	var completed = courses[icourse].percent_complete;
